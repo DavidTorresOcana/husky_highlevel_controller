9302b2d401ece18f8849567e3fb895c1cb6b6d41
DavidTorresOcana <david.torres@dbxdrones.com>
2017-09-10 20:34:48 +0100

    Points from 1 to 8 implemented. Not compiling

--- include/husky_highlevel_controller/HuskyHighlevelController.hpp
+++ include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -1,26 +1,44 @@
 #pragma once
 
 #include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <sensor_msgs/LaserScan.h>
 
 namespace husky_highlevel_controller {
 
 /*!
  * Class containing the Husky Highlevel Controller
  */
-class HuskyHighlevelController {
-public:
-	/*!
-	 * Constructor.
-	 */
-	HuskyHighlevelController(ros::NodeHandle& nodeHandle);
+	class HuskyHighlevelController {
+		private:
+			ros::NodeHandle nodeHandle_;
+			ros::Subscriber sub_scan_;
+			std::string sub_topic_name;
+			int queue_size;
+		public:
+			/*!
+			 * Constructor.
+			 */
+			HuskyHighlevelController(ros::NodeHandle& nodeHandle);
 
-	/*!
-	 * Destructor.
-	 */
-	virtual ~HuskyHighlevelController();
+			/*!
+			 * Destructor.
+			 */
+			virtual ~HuskyHighlevelController();
+			/*!
+			 * Welcome.
+			 */
+			virtual void Welcome();
+			/*!
+			 * Subscriptions.
+			 */
+			virtual void subscription_gen();
+			/*!
+			 * Callback subscriber.
+			 */
+			virtual void scanCallback(const sensor_msgs::LaserScan &scan);
 
-private:
-	ros::NodeHandle nodeHandle_;
-};
+
+	};
 
 } /* namespace */
--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -2,13 +2,26 @@
 
 namespace husky_highlevel_controller {
 
-HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) :
-  nodeHandle_(nodeHandle)
-{
-}
+	HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) :
+	  nodeHandle_(nodeHandle)
+	{
+		
+	}
 
-HuskyHighlevelController::~HuskyHighlevelController()
-{
-}
-
+	HuskyHighlevelController::~HuskyHighlevelController()
+	{
+		
+	}
+	void HuskyHighlevelController::Welcome(){
+		ROS_INFO_STREAM("Starting: "<<ros::this_node::getNamespace());
+	}
+	void HuskyHighlevelController::subscription_gen(){
+		if (!nodeHandle_.getParam("scan_subscriber_topic_name", sub_topic_name)) {ROS_ERROR("Could not find topic parameter!"); }
+		if (!nodeHandle_.getParam("scan_subscriber_queue_size", queue_size)) {ROS_ERROR("Could not find topic parameter!"); }
+		sub_scan_ = nodeHandle_.subscribe(sub_topic_name,queue_size,&HuskyHighlevelController::scanCallback,this);
+	}
+	void HuskyHighlevelController::scanCallback(const sensor_msgs::LaserScan &scan){
+		ROS_INFO_STREAM("Minimum laser distance (m): ", scan.range_min);
+	}
+	
 } /* namespace */
