119e313c897818312cabefcdbb4a6e1c033138b4
DavidTorresOcana <david.torres@dbxdrones.com>
2017-09-20 20:18:54 +0100

    ROS course exercise 3. TF listeners, publishing data, implementing control loop, etc

--- include/husky_highlevel_controller/HuskyHighlevelController.hpp
+++ include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -3,6 +3,10 @@
 #include <ros/ros.h>
 #include "std_msgs/String.h"
 #include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
+#include <visualization_msgs/Marker.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
 
 namespace husky_highlevel_controller {
 
@@ -12,9 +16,21 @@ namespace husky_highlevel_controller {
 	class HuskyHighlevelController {
 		private:
 			ros::NodeHandle nodeHandle_;
+
 			ros::Subscriber sub_scan_;
+
+			ros::Publisher pub_vel_cmd_;
+			geometry_msgs::Twist msg_vel_cmd_;
+			ros::Publisher vis_pub;
+			visualization_msgs::Marker marker;
+
+			tf2_ros::Buffer tfBuffer;
+			tf2_ros::TransformListener tfListener(tfBuffer);
+			geometry_msgs::TransformStamped transformStamped;
+
 			std::string sub_topic_name;
 			int queue_size;
+			float min_range, ang_min, x_pillar, y_pillar, ang_p_gain,lin_vel,ang2rob,r2rob;
 		public:
 			/*!
 			 * Constructor.
@@ -37,6 +53,14 @@ namespace husky_highlevel_controller {
 			 * Callback subscriber.
 			 */
 			virtual void scanCallback(const sensor_msgs::LaserScan &scan);
+			/*!
+       * Publishers.
+       */
+      virtual void publishers_gen();
+      /*!
+       * Load parameters.
+       */
+      virtual void Load_param();
 
 
 	};
--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -8,9 +8,12 @@ namespace husky_highlevel_controller {
 
 	  // Welcome
 	  HuskyHighlevelController::Welcome();
-
+	  // Load parameters from server
+      HuskyHighlevelController::Load_param();
 	  // Subscriptions
 	  HuskyHighlevelController::subscription_gen();
+	  // Publishers
+    HuskyHighlevelController::publishers_gen();
 	}
 
 	HuskyHighlevelController::~HuskyHighlevelController()
@@ -18,23 +21,72 @@ namespace husky_highlevel_controller {
 		
 	}
 	void HuskyHighlevelController::Welcome(){
-		ROS_INFO_STREAM("Starting: "<<ros::this_node::getName());
+		  ROS_INFO_STREAM("Starting: "<<ros::this_node::getName());
 	}
+	void HuskyHighlevelController::Load_param(){
+	    if (!nodeHandle_.getParam("scan_subscriber_topic_name", sub_topic_name)) {ROS_ERROR("Could not find topic parameter!"); }
+      if (!nodeHandle_.getParam("scan_subscriber_queue_size", queue_size)) {ROS_ERROR("Could not find topic parameter!"); }
+      if (!nodeHandle_.getParam("angular_ctrl_p_gain", ang_p_gain)) {ROS_ERROR("Could not find topic parameter!"); }
+      if (!nodeHandle_.getParam("linear_ctrl_vel", lin_vel)) {ROS_ERROR("Could not find topic parameter!"); }
+  }
 	void HuskyHighlevelController::subscription_gen(){
-		if (!nodeHandle_.getParam("scan_subscriber_topic_name", sub_topic_name)) {ROS_ERROR("Could not find topic parameter!"); }
-		if (!nodeHandle_.getParam("scan_subscriber_queue_size", queue_size)) {ROS_ERROR("Could not find topic parameter!"); }
-		sub_scan_ = nodeHandle_.subscribe(sub_topic_name, queue_size,&HuskyHighlevelController::scanCallback,this);
-	}
-	void HuskyHighlevelController::scanCallback(const sensor_msgs::LaserScan &scan){
 
-		  float minn=10000;
+      sub_scan_ = nodeHandle_.subscribe(sub_topic_name, queue_size,&HuskyHighlevelController::scanCallback,this);
+	}
+	void HuskyHighlevelController::publishers_gen(){
+      pub_vel_cmd_ = nodeHandle_.advertise<geometry_msgs::Twist>("/cmd_vel",10);
+      vis_pub = nodeHandle_.advertise<visualization_msgs::Marker>("/visualization_marker", 0 );
+	 }
+	void HuskyHighlevelController::scanCallback(const sensor_msgs::LaserScan &scan){
+		  min_range=10000;
 
 		  for(int i=0;i<floor((scan.angle_max-scan.angle_min)/scan.angle_increment);i++){
-				if(minn>scan.ranges[i]){
-				  minn = scan.ranges[i];
-				}
+          if(min_range>scan.ranges[i]){
+            min_range = scan.ranges[i];
+            ang_min = scan.angle_min + scan.angle_increment*i;
+          }
 			  }
-		ROS_INFO_STREAM("Minimum laser distance (m): "<<minn);
+		  //ROS_INFO_STREAM("Minimum laser distance (m): "<<min_range);
 
+		  // Relative position to the pillar x = min_range*cos(ang_min), y = min_range*sin(ang_min)
+		  x_pillar = min_range*cos(ang_min);
+		  y_pillar = min_range*sin(ang_min);
+		  ROS_INFO_STREAM("\n x = "<<x_pillar<<"\n y = "<<y_pillar);
+		  // Compute vel command to hit the pillar
+		  msg_vel_cmd_.linear.x = lin_vel;
+		  msg_vel_cmd_.angular.z =  ang_p_gain*(0-ang_min);
+
+		  // Publish the command
+		  pub_vel_cmd_.publish(msg_vel_cmd_);
+
+		  // Publish the marker of the obstacle
+		  marker.header.frame_id = "base_laser";
+		  marker.header.stamp = ros::Time();
+		  marker.ns = "Pillar";
+		  marker.id = 0;
+		  marker.type = visualization_msgs::Marker::SPHERE;
+		  marker.action = visualization_msgs::Marker::ADD;
+		  marker.pose.position.x = x_pillar;
+		  marker.pose.position.y = y_pillar;
+		  marker.pose.position.z = 0;
+		  marker.scale.x = 1;
+		  marker.scale.y = 1;
+		  marker.scale.z = 10;
+		  marker.color.a = 1.0; // Don't forget to set the alpha!
+		  marker.color.r = 0.0;
+		  marker.color.g = 1.0;
+		  marker.color.b = 0.0;
+		  //vis_pub.publish( marker );
+		  // Publish marker positions, with diferent method
+		  marker.header.frame_id = "odom";
+		  try { transformStamped = tfBuffer.lookupTransform("base_laser","odom", ros::Time(0));  }
+		  catch (tf2::TransformException &exception) { ROS_WARN("%s", exception.what()); ros::Duration(1.0).sleep(); continue;  }
+		  ang2rob = atan2(transformStamped.transform.translation.y, transformStamped.transform.translation.x);
+		  r2rob = sqrt(transformStamped.transform.translation.y*transformStamped.transform.translation.y+ transformStamped.transform.translation.x* transformStamped.transform.translation.x);
+
+		  marker.pose.position.x = r2rob*cos(ang2rob)+min_range*cos(ang_min+ang2rob);
+		  marker.pose.position.y = r2rob*sin(ang2rob)+min_range*sin(ang_min+ang2rob);
+
+		  vis_pub.publish( marker );
 	}
 } /* namespace */
